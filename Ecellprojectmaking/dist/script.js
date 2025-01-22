d9dd3e9b46556a86f1b3c0996fff8959589e2d14
harsherchauhan18 <harshvardhanchauhan719@gmail.com>
2025-01-23 02:23:08 +0530

    Add files via upload

---
+++ Ecellprojectmaking/dist/script.js
@@ -0,0 +1,10 @@
+const navdilog=document.getElementById('nav-dialog')
+function handlemenu(){
+    navdilog.classList.toggle("hidden")
+}
+const boxes = document.querySelectorAll(".blue"); // Use class selector instead of ID
+boxes.forEach(box=>{
+    box.addEventListener("click",()=>{
+        box.classList.toggle('red')
+    })
+})
